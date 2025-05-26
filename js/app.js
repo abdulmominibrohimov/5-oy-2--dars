91b1f87927b2f0be486e28904bb982d6f26f7556
a <a>
2025-05-26 17:49:46 +0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,111 +1,132 @@
 
+
+
+
 document.body.style.backgroundColor = "#141414"
 const container = document.createElement("div");
 container.style.backgroundColor = "#1f1f1f";
-container.style.color = "white";
-container.style.width = "384px";
-container.style.height = "611px";
-container.style.margin = "174px 528px";
-container.style.padding = "40px";
-container.style.borderRadius = "12px";
-container.style.gap = "24px"
-container.style.fontFamily = "Arial, sans-serif";
-container.style.textAlign = "center";
-document.body.appendChild(container);
 
+
+const container2 = document.createElement("div");
+document.body.append(container2);
+
+container2.style.cssText = `
+background-color: #1F1F1F;
+width: 384px;
+height: 611px;
+display: flex;
+flex-direction: column;
+padding: 40px;
+gap: 8px;
+margin: 0 auto;
+borderRadius = "12px";
+`
 
 
 const image = document.createElement("img");
-container.appendChild(image);
-image.src = "./images/profile-pic.png"; 
-image.style.cssText = `width = "88px";
-image.style.height = "88px";
-image.style.borderRadius = "50%";
-image.style.marginBottom = "2px";
-image.style.marginTop = "0"`
+document.body.append(image);
+image.src = "./images/profile-pic.png";
+container2.append(image);
+image.style.cssText = `
+width: 88px;
+height: 88px;
+borderRadius = "50%";
+margin: 0 auto;
+marginBottom = "2px";
+`
 
-
-
-const fulname = document.createElement("h2");
-container.appendChild(fulname);
+const fulname = document.createElement("h1");
 fulname.innerText = "Jessica Randall";
-fulname.style.cssText = `marginBottom = "-15px";
-fulname.style.fontSize = "33px"
-fulname.style.lineHeight = "150%"
-fulname.style.fontWeight = "600"`
+container2.append(fulname);
+fulname.style.cssText = `
+color: white;
+font-size: 33px;
+margin: 0 auto;
+margin-top: 20px;
+lineHeight = "150%"
+fontWeight = "600"
+`
 
-
-
-const user = document.createElement("p");
-container.appendChild(user)
+const user = document.createElement("h2");
+container2.append(user);
 user.innerText = "London, United Kingdom";
-user.style.cssText = `color = " #C4F82A";
-user.style.marginBottom = "28px";
-user.style.fontWeight = "700"
-user.style.fontSize = "20px"`
+user.style.cssText = `
+color: #C4F82A;
+margin: 0 auto;
+margin-top: 4px;
+marginBottom = "28px";
+fontWeight = "700
+font-size: 15px;
+`
 
-
-
-const bio = document.createElement("p");
-container.appendChild(bio);
-bio.innerText = `"Front-end developer and avid reader."`;
-bio.style.cssText = `fontSize = "20px";
-bio.style.marginBottom = "24px";`
-
+const job = document.createElement("p");
+job.innerText = "Front - end developer and avid reader.";
+container2.append(job);
+job.style.cssText = `
+font-size: 21px;
+color: white;
+margin: 0 auto;
+margin-top: 8px;
+`
 
 const button1 = document.createElement("button");
 button1.innerText = "github";
-container.append(button1);
+container2.append(button1);
 button1.style.cssText = `
 background-color: #333333;
 border-radius: 8px;
 padding: 12px;
-margin-top: 24px;
-color: #FFFFFF;
+margin-top: 12px;
+border: none;
+color: white;
 font-size: 16px;
 `
 
 const button2 = document.createElement("button");
 button2.innerText = "Frontend Mentor";
-container.append(button2);
+container2.append(button2);
 button2.style.cssText = `
 background-color: #333333;
 border-radius: 8px;
 padding: 12px;
-margin-top: 24px;
+margin-top: 12px;
+border: none;
 color: #FFFFFF;
 font-size: 16px;
 `
 const button3 = document.createElement("button");
 button3.innerText = "LinkedIn";
-container.append(button3);
+container2.append(button3);
 button3.style.cssText = `
 background-color: #333333;
 border-radius: 8px;
 padding: 12px;
-margin-top: 24px;
+margin-top: 12px;
+border: none;
 color: #FFFFFF;
 font-size: 16px;
 `
 const button4 = document.createElement("button");
 button4.innerText = "twitter";
-container.append(button4);
+container2.append(button4);
 button4.style.cssText = `
 background-color: #333333;
 border-radius: 8px;
 padding: 12px;
-margin-top: 24px;
+margin-top: 12px;
+border: none;
 color: #FFFFFF;
 font-size: 16px;
 `
 const button5 = document.createElement("button");
 button5.innerText = "instagram";
-container.append(button5);
+container2.append(button5);
 button5.style.cssText = `
 background-color: #333333;
 border-radius: 8px;
 padding: 12px;
-margin-top: 24px;
+margin-top: 12px;
+border: none;
 color: #FFFFFF;
 font-size: 16px;
 `
